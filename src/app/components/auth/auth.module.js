82622d9a50b70475ab26ec2daeefc42d78e3d937
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2017-02-18 18:50:32 -0400

    Initialize fireabse credentials

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -2,4 +2,21 @@ angular
   .module('components.auth', [
     'ui.router',
     'firebase'
-  ]);
+  ])
+  .config(function($firebaseRefProvider){
+    var config = {
+      apiKey: "AIzaSyDW1kDUOCG5psHe8UuaKKEsw-TrDqA74Qg",
+      authDomain: "contacts-manager-8847d.firebaseapp.com",
+      databaseURL: "https://contacts-manager-8847d.firebaseio.com",
+      storageBucket: "contacts-manager-8847d.appspot.com",
+      messagingSenderId: "892102538580"
+    };
+
+    $firebaseRefProvider
+      .registerUrl({
+        default: config.databaseURL,
+        contacts: config.databaseURL + '/contacts'
+      });
+
+    firebase.initializeApp(config);
+  });
